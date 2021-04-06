b2285fc975d87fbf2b7abb141aee01b31a3ae778
jonas.hall <jonas.hall@uranus.uni-freiburg.de>
2021-04-06 17:00:46 +0200

    feat: Include 2D test with a simple constraint matrix
    Succeeded first try

---
+++ examples/warm_up_w_A.cpp
@@ -0,0 +1,70 @@
+/*
+ *	This file is part of lcqpOASES.
+ *
+ *	lcqpOASES -- A Solver for Quadratic Programs with Commplementarity Constraints.
+ *	Copyright (C) 2020 - 2021 by Jonas Hall et al.
+ *
+ *	lcqpOASES is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU Lesser General Public
+ *	License as published by the Free Software Foundation; either
+ *	version 2.1 of the License, or (at your option) any later version.
+ *
+ *	lcqpOASES is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *	See the GNU Lesser General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Lesser General Public
+ *	License along with lcqpOASES; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+#include <iostream>
+#include "LCQProblem.hpp"
+
+using namespace lcqpOASES;
+
+int main() {
+    std::cout << "Preparing warm up problem...\n";
+
+    /* Setup data of first QP. */
+    double H[2*2] = { 2.0, 0.0, 0.0, 2.0 };
+    double g[2] = { -2.0, -2.0 };
+    double lb[2] = { 0, 0 };
+    double ub[2] = { INFINITY, INFINITY };
+    double S1[1*2] = {1.0, 0.0};
+    double S2[1*2] = {0.0, 1.0};
+    double A[1*2] = {1.0, -1.0};
+    double lbA[1] = { -0.5 };
+    double ubA[1] = {  INFINITY };
+
+    int nV = 2;
+    int nC = 1;
+    int nComp = 1;
+
+    LCQProblem lcqp( nV, nC, nComp );
+
+	Options options;
+    options.printLvl = printLevel::INNER_LOOP_ITERATES;
+	lcqp.setOptions( options );
+
+    // Solve first LCQP
+	returnValue retVal = lcqp.solve( H, g, lb, ub, S1, S2, A, lbA, ubA );
+
+    if (retVal != SUCCESSFUL_RETURN)
+    {
+        printf("Termination ended without success.\n");
+        return 1;
+    }
+
+    // Get solutions
+    double* xOpt = new double[2];
+	double* yOpt = new double[nV + nC + 2*nComp];
+	lcqp.getPrimalSolution( xOpt );
+	lcqp.getDualSolution( yOpt );
+	printf( "\nxOpt = [ %g, %g ];  yOpt = [ %g, %g, %g, %g ]; \n\n",
+			xOpt[0],xOpt[1],yOpt[0],yOpt[1],yOpt[2],yOpt[3] );
+
+    return 0;
+}
