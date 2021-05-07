7e69244e1469480bdd9b411c4975fca074ad92d3
Jonas <hall.f.jonas@gmail.com>
2021-05-07 16:56:54 +0200

    feat: addpath to interface

--- interfaces/matlab/examples/warm_up.m
+++ interfaces/matlab/examples/warm_up.m
@@ -2,6 +2,8 @@
 % Clean up and load libraries
 clear all; clc; close all; 
 
+addpath(fullfile(pwd, ".."));
+
 % Simple QP 
 Q = [2 0; 0 2];
 g = [-2; -2];
@@ -9,7 +11,7 @@ L = [1 0];
 R = [0 1];
 
 % Run solver
-LCQParrot(Q, g, L, R);
+x0 = LCQPanther(Q, g, L, R);
 
 % Algorithm parameters
 params.x0 = [1; 1];
