97931c41d733886e8544bf58618d1495defd4e56
Jonas <hall.f.jonas@gmail.com>
2021-05-11 17:23:22 +0200

    feat: Include parameter passing in matlab interface

--- interfaces/matlab/LCQPanther.cpp
+++ interfaces/matlab/LCQPanther.cpp
@@ -21,13 +21,13 @@
 
 #include "LCQProblem.hpp"
 using lcqpOASES::LCQProblem;
-using lcqpOASES::Utilities;
+using lcqpOASES::Options;
 
 #include <mex.h>
 
 LCQProblem lcqp;
 
-bool checkDimensionAndType(const mxArray* arr, size_t m, size_t n, const char* name)
+bool checkDimensionAndTypeDouble(const mxArray* arr, size_t m, size_t n, const char* name)
 {
     if (!mxIsDouble(arr)) {
         char* errorMsg = (char*)malloc(100*sizeof(char));
@@ -48,7 +48,28 @@ bool checkDimensionAndType(const mxArray* arr, size_t m, size_t n, const char* n
     return true;
 }
 
-bool checkStructType(const mxArray* arr, const char* name)
+bool checkDimensionAndTypeBool(const mxArray* arr, size_t m, size_t n, const char* name)
+{
+    if (!mxIsLogical(arr)) {
+        char* errorMsg = (char*)malloc(100*sizeof(char));
+        sprintf(errorMsg, "Invalid type: %s must be of type logical.\n", name);
+        mexErrMsgTxt(errorMsg);
+        free(errorMsg);
+        return false;
+    }
+
+    if (mxGetM(arr) != m || mxGetN(arr) != n) {
+        char* errorMsg = (char*)malloc(100*sizeof(char));
+        sprintf(errorMsg, "Invalid dimension: %s (got %d x %d but expected %d x %d).\n", name, (int)mxGetM(arr), (int)mxGetN(arr), (int)m, (int)n);
+        mexErrMsgTxt(errorMsg);
+        free(errorMsg);
+        return false;
+    }
+
+    return true;
+}
+
+bool checkTypeStruct(const mxArray* arr, const char* name)
 {
     if (!mxIsStruct(arr)) {
         char* errorMsg = (char*)malloc(100*sizeof(char));
@@ -59,6 +80,17 @@ bool checkStructType(const mxArray* arr, const char* name)
     }
 
     return true;
+}
+
+void printOptions( Options options ) {
+    mexPrintf(" \n Using LCQPanther Options: \n");
+    mexPrintf("          rho0: %g \n", options.getInitialComplementarityPenalty());
+    mexPrintf("          beta: %g \n", options.getComplementarityPenaltyUpdate());
+    mexPrintf("     compl tol: %g \n", options.getComplementarityTolerance());
+    mexPrintf("     stati tol: %g \n", options.getStationarityTolerance());
+    mexPrintf("      max iter: %d \n", options.getMaxIterations());
+    mexPrintf("solve zero pen: %d \n", (int) options.getSolveZeroPenaltyFirst());
+    mexPrintf("          prnt: %d \n\n", options.getPrintLevel());
 }
 
 void mexFunction( int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[] )
@@ -126,33 +158,33 @@ void mexFunction( int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[] )
     // mexPrintf("Got LCQP of dimension (nV x nComp x nC) = (%d x %d x %d).\n", nV, nComp, nC);
 
     // Check all dimensions (except for params)
-    if (!checkDimensionAndType(prhs[0], nV, nV, "H")) return;
-    if (!checkDimensionAndType(prhs[1], nV, 1, "g")) return;
-    if (!checkDimensionAndType(prhs[2], nComp, nV, "S1")) return;
-    if (!checkDimensionAndType(prhs[3], nComp, nV, "S2")) return;
+    if (!checkDimensionAndTypeDouble(prhs[0], nV, nV, "H")) return;
+    if (!checkDimensionAndTypeDouble(prhs[1], nV, 1, "g")) return;
+    if (!checkDimensionAndTypeDouble(prhs[2], nComp, nV, "S1")) return;
+    if (!checkDimensionAndTypeDouble(prhs[3], nComp, nV, "S2")) return;
 
-    if (nrhs == 6 && !checkDimensionAndType(prhs[4], nV, 1, "lb")) return;
-    if (nrhs == 6 && !checkDimensionAndType(prhs[5], nV, 1, "ub")) return;
+    if (nrhs == 6 && !checkDimensionAndTypeDouble(prhs[4], nV, 1, "lb")) return;
+    if (nrhs == 6 && !checkDimensionAndTypeDouble(prhs[5], nV, 1, "ub")) return;
 
-    if (nrhs == 7 && mxIsStruct(prhs[6]) && !checkDimensionAndType(prhs[4], nV, 1, "lb")) return;
-    if (nrhs == 7 && mxIsStruct(prhs[6]) && !checkDimensionAndType(prhs[5], nV, 1, "ub")) return;
+    if (nrhs == 7 && mxIsStruct(prhs[6]) && !checkDimensionAndTypeDouble(prhs[4], nV, 1, "lb")) return;
+    if (nrhs == 7 && mxIsStruct(prhs[6]) && !checkDimensionAndTypeDouble(prhs[5], nV, 1, "ub")) return;
 
-    if (nrhs == 7 && !mxIsStruct(prhs[6]) && !checkDimensionAndType(prhs[4], nC, nV, "A")) return;
-    if (nrhs == 7 && !mxIsStruct(prhs[6]) && !checkDimensionAndType(prhs[5], nC, 1, "lbA")) return;
-    if (nrhs == 7 && !mxIsStruct(prhs[6]) && !checkDimensionAndType(prhs[6], nC, 1, "ubA")) return;
+    if (nrhs == 7 && !mxIsStruct(prhs[6]) && !checkDimensionAndTypeDouble(prhs[4], nC, nV, "A")) return;
+    if (nrhs == 7 && !mxIsStruct(prhs[6]) && !checkDimensionAndTypeDouble(prhs[5], nC, 1, "lbA")) return;
+    if (nrhs == 7 && !mxIsStruct(prhs[6]) && !checkDimensionAndTypeDouble(prhs[6], nC, 1, "ubA")) return;
 
-    if ((nrhs == 8 || nrhs == 9) && !checkDimensionAndType(prhs[4], nC, nV, "A")) return;
-    if ((nrhs == 8 || nrhs == 9) && !checkDimensionAndType(prhs[5], nC, 1, "lbA")) return;
-    if ((nrhs == 8 || nrhs == 9) && !checkDimensionAndType(prhs[6], nC, 1, "ubA")) return;
+    if ((nrhs == 8 || nrhs == 9) && !checkDimensionAndTypeDouble(prhs[4], nC, nV, "A")) return;
+    if ((nrhs == 8 || nrhs == 9) && !checkDimensionAndTypeDouble(prhs[5], nC, 1, "lbA")) return;
+    if ((nrhs == 8 || nrhs == 9) && !checkDimensionAndTypeDouble(prhs[6], nC, 1, "ubA")) return;
 
-    if ((nrhs == 9 || nrhs == 10) && !checkDimensionAndType(prhs[7], nV, 1, "lb")) return;
-    if ((nrhs == 9 || nrhs == 10) && !checkDimensionAndType(prhs[8], nV, 1, "ub")) return;
+    if ((nrhs == 9 || nrhs == 10) && !checkDimensionAndTypeDouble(prhs[7], nV, 1, "lb")) return;
+    if ((nrhs == 9 || nrhs == 10) && !checkDimensionAndTypeDouble(prhs[8], nV, 1, "ub")) return;
 
     // Check structs
-    if (nrhs == 5 && !checkStructType(prhs[4], "params")) return;
-    if (nrhs == 7 && !mxIsDouble(prhs[6]) && !checkStructType(prhs[6], "params")) return;
-    if (nrhs == 8 && !checkStructType(prhs[7], "params")) return;
-    if (nrhs == 10 && !checkStructType(prhs[9], "params")) return;
+    if (nrhs == 5 && !checkTypeStruct(prhs[4], "params")) return;
+    if (nrhs == 7 && !mxIsDouble(prhs[6]) && !checkTypeStruct(prhs[6], "params")) return;
+    if (nrhs == 8 && !checkTypeStruct(prhs[7], "params")) return;
+    if (nrhs == 10 && !checkTypeStruct(prhs[9], "params")) return;
 
     // Sparsity checks
     if (mxIsSparse(prhs[0]) || (nC > 0 && mxIsSparse(prhs[4]))) {
@@ -160,8 +192,9 @@ void mexFunction( int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[] )
         return;
     }
 
-    // Create LCQP object
+    // Create LCQP and options objects
     LCQProblem lcqp((int)nV, (int)nC, (int)nComp);
+    Options options;
 
     // Load data
     double* H = NULL;
@@ -173,7 +206,8 @@ void mexFunction( int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[] )
     double* A = NULL;
     double* lbA = NULL;
     double* ubA = NULL;
-    // Options opts();
+    double* x0 = NULL;
+    double* y0 = NULL;
 
     H = (double*) mxGetPr( prhs[0] );
     g = (double*) mxGetPr( prhs[1] );
@@ -196,18 +230,103 @@ void mexFunction( int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[] )
 
     // Load settings
     int structIdx = -1;
-    if (nrhs == 5 && checkStructType(prhs[4], "params")) { structIdx = 4; }
-    if (nrhs == 7 && !mxIsDouble(prhs[6]) && checkStructType(prhs[6], "params"))  { structIdx = 6; }
-    if (nrhs == 8 && checkStructType(prhs[7], "params")) { structIdx = 7; }
-    if (nrhs == 10 && checkStructType(prhs[9], "params")) { structIdx = 9; }
+    if (nrhs == 5 && checkTypeStruct(prhs[4], "params")) { structIdx = 4; }
+    if (nrhs == 7 && !mxIsDouble(prhs[6]) && checkTypeStruct(prhs[6], "params"))  { structIdx = 6; }
+    if (nrhs == 8 && checkTypeStruct(prhs[7], "params")) { structIdx = 7; }
+    if (nrhs == 10 && checkTypeStruct(prhs[9], "params")) { structIdx = 9; }
 
     if (structIdx != -1) {
-        mexErrMsgTxt("Passing the parameter field is not yet supported.\n");
-        return;
+
+        const char* params_fieldnames[] = {
+            "x0",
+            "stationarityTolerance",
+            "complementarityTolerance",
+            "initialComplementarityPenalty",
+            "complementarityPenaltyUpdate",
+            "solveZeroPenaltyFirst",
+            "maxIterations",
+            "printLvl"
+        };
+
+        for (auto name : params_fieldnames) {
+            mxArray* field = mxGetField(prhs[structIdx], 0, name);
+            double* fld_ptr;
+            bool* fld_ptr_bool;
+
+            if (field == NULL)
+                continue;
+
+            if ( name == "stationarityTolerance") {
+                if (!checkDimensionAndTypeDouble(field, 1, 1, "params.stationarityTolerance")) return;
+
+                fld_ptr = (double*) mxGetPr(field);
+                options.setStationarityTolerance( fld_ptr[0] );
+                continue;
+            }
+
+            if ( name == "complementarityTolerance") {
+                if (!checkDimensionAndTypeDouble(field, 1, 1, "params.complementarityTolerance")) return;
+
+                fld_ptr = (double*) mxGetPr(field);
+                options.setComplementarityTolerance( fld_ptr[0] );
+                continue;
+            }
+
+            if ( name == "initialComplementarityPenalty") {
+                if (!checkDimensionAndTypeDouble(field, 1, 1, "params.initialComplementarityPenalty")) return;
+
+                fld_ptr = (double*) mxGetPr(field);
+                options.setInitialComplementarityPenalty( fld_ptr[0] );
+                continue;
+            }
+
+            if ( name == "complementarityPenaltyUpdate") {
+                if (!checkDimensionAndTypeDouble(field, 1, 1, "params.complementarityPenaltyUpdate")) return;
+
+                fld_ptr = (double*) mxGetPr(field);
+                options.setComplementarityPenaltyUpdate( fld_ptr[0] );
+                continue;
+            }
+
+            if ( name == "solveZeroPenaltyFirst") {
+                if (!checkDimensionAndTypeBool(field, 1, 1, "params.solveZeroPenaltyFirst")) return;
+
+                fld_ptr_bool = (bool*) mxGetPr(field);
+                options.setSolveZeroPenaltyFirst( fld_ptr[0] );
+                continue;
+            }
+
+            if ( name == "maxIterations") {
+                if (!checkDimensionAndTypeDouble(field, 1, 1, "params.maxIterations")) return;
+
+                fld_ptr = (double*) mxGetPr(field);
+                options.setMaxIterations( (int)fld_ptr[0] );
+                continue;
+            }
+
+            if ( name == "printLvl") {
+                if (!checkDimensionAndTypeDouble(field, 1, 1, "params.printLvl")) return;
+
+                fld_ptr = (double*) mxGetPr(field);
+                options.setPrintLevel( (int)fld_ptr[0] );
+                continue;
+            }
+
+            if ( name == "x0") {
+                if (!checkDimensionAndTypeDouble(field, nV, 1, "params.x0")) return;
+
+                x0 = (double*) mxGetPr(field);
+                continue;
+            }
+        }
+
+        lcqp.setOptions( options );
     }
 
+    printOptions( options );
+
     // Load data into LCQP object
-    lcqp.loadLCQP(H, g, S1, S2, A, lbA, ubA, lb, ub);
+    lcqp.loadLCQP(H, g, S1, S2, A, lbA, ubA, lb, ub, x0);
 
     // Run solver
     lcqpOASES::returnValue ret = lcqp.runSolver();
