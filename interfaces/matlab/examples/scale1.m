df9a3bf18b5bb1076bc1b2b833b733392e04ead4
Jonas <hall.f.jonas@gmail.com>
2021-05-14 11:49:10 +0200

    feat: Include scaled problem requiring large multiplier

---
+++ interfaces/matlab/examples/scale1.m
@@ -0,0 +1,31 @@
+%% Run a simple warm up problem
+% Clean up and load libraries
+clear all; clc; close all;
+
+addpath(fullfile(pwd, ".."));
+
+% Simple QP
+Q = [2*100^2 0; 0 2];
+g = [-200; -2];
+L = [1 0];
+R = [0 1];
+
+% Algorithm parameters (C++)
+params.x0 = [1; 1];
+params.initialPenaltyParameter = 10;
+params.penaltyUpdateFactor = 2;
+params.solveZeroPenaltyFirst = true;
+params.printLevel = 2;
+
+% Run solver
+nexp = 1;
+for i = 1:nexp
+    tic;
+    [xopt, yopt, stats] = LCQPanther(Q, g, L, R, params);
+    times_c(i) = toc;
+
+    %tic;
+    %LCQP(Q, g, A, lb, ub, lbA, ubA, L, R, params_mat)
+    %times_m(i) = toc;
+end
+
