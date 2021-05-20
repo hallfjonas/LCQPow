fef5ff74adf428e734739cbb0f7928f9ca7608f4
Jonas <hall.f.jonas@gmail.com>
2021-05-20 14:51:49 +0200

    feat: Self contained example (including plot functions)

--- interfaces/matlab/examples/optimize_on_circle.m
+++ interfaces/matlab/examples/optimize_on_circle.m
@@ -10,7 +10,7 @@ addpath(fullfile(pwd, ".."));
 %
 
 % Circle discretization
-N = 5;
+N = 200;
 nz = 2 + 2*N;
 
 Qx = [17 -15; -15 17];
@@ -50,10 +50,17 @@ end
 params.x0 = ones(nz,1); params.x0(1:2) = xk;
 params.initialPenaltyParameter = 0.001;
 params.penaltyUpdateFactor = 2;
-params.solveZeroPenaltyFirst = false;
-params.printLevel = 2;
+params.solveZeroPenaltyFirst = true;
+params.printLevel = 0;
 
-[xOpt,~,stats] = LCQPanther(Q, g, L, R, A, lbA, ubA, lb, ub, params);
+% Use sparse solver
+params.qpSolver = 1;
+if (~issparse(Q))
+    Q = sparse(Q);
+    L = sparse(L);
+    R = sparse(R);
+    A = sparse(A);
+end
 
 % Set to latex (only relevant for plotting)
 set(groot,'defaultAxesTickLabelInterpreter','latex');
@@ -62,54 +69,111 @@ set(groot,'defaultLegendInterpreter','latex');
 
 %% Performance test
 % Call solver for various rho init values
-rho0 = [1.5*eps];
+rho0 = [10e-8];
 while(rho0(end) < 10^2)
     rho0 = [rho0, rho0(end)*1.5];
 end
 
-time_vals = [];
-obj_vals = [];
-iter_vals = [];
-outer_vals = [];
+c_solver = struct;
+m_solver = struct;
+
+plot_maps = containers.Map;
+
+c_solver.time_vals = zeros(length(rho0),1);
+c_solver.obj_vals = zeros(length(rho0),1);
+c_solver.iter_vals = zeros(length(rho0),1);
+c_solver.outer_vals = zeros(length(rho0),1);
+
 for i = 1:length(rho0)
     params.initialPenaltyParameter = rho0(i);
 
+    % Run c solver
     tic;
     [xOpt,~,stats] = LCQPanther(Q, g, L, R, A, lbA, ubA, lb, ub, params);
 
     % Evaluation
-    time_vals = [time_vals, toc];
-    obj_vals = [obj_vals, obj(xOpt(:,end))];
-    iter_vals = [iter_vals, stats.iters_total];
-    outer_vals = [outer_vals, stats.iters_outer];
+    c_solver.time_vals(i) = toc;
+    c_solver.obj_vals(i) = obj(xOpt(:,end));
+    c_solver.iter_vals(i) = stats.iters_total;
+    c_solver.outer_vals(i) = stats.iters_outer;
 end
 
-%% Performance Plot
-CreatePerformancePlot(rho0, time_vals, obj_vals, iter_vals, outer_vals);
+plot_maps("C++") = c_solver;
 
-%% Step Plot
-params.storeSteps = true;
-params.printStats = true;
+% Performance Plot
+CreatePerformancePlot(rho0, plot_maps);
 
-rho0 = [1, 10, 12.4094];
-CreateEdgesPlot(xk, N, Qx);
-
-% global minimum
-params.R0 = rho0(1);
-xOpt_1 = LCQPanther(Q, g, A, lb, ub, lbA, ubA, L, R, params);
-AddSteps(xOpt_1, "$\rho_0 = 1$", 'b', 'x');
-
-% (strict) local minimum
-params.R0 = rho0(2);
-xOpt_10 = LCQPanther(Q, g, A, lb, ub, lbA, ubA, L, R, params);
-AddSteps(xOpt_10, "$\rho_0 = 10$", 'r', 'x');
-
-% Uncomment to plot this M-stationary point
-%params.R0 = rho0(3);
-%xOpt_12 = LCQP(Q, g, A, lb, ub, lbA, ubA, L, R, params);
-%AddSteps(xOpt_12, "$\rho_0 = 12.4094$", 'g', 'o');
-
-saveas(gcf,strcat('steps', '.png'));
-
-%% Objective versus angle
+% Objective versus angle
 ObjectiveVersusAngle(obj);
+
+%% Plot Functions
+function[] = CreatePerformancePlot(rho, plot_maps)
+
+names = plot_maps.keys;
+
+figure;
+subplot(2,2,1); grid on; box on;
+for i = 1:length(names)
+    plot_struct = plot_maps(names{i});
+    loglog(rho, plot_struct.time_vals); hold on;
+end
+ylabel("CPU time in [s]");
+xlabel("Initial penalty value");
+legend(names);
+
+subplot(2,2,2); grid on; box on;
+for i = 1:length(names)
+    plot_struct = plot_maps(names{i});
+    loglog(rho, plot_struct.obj_vals); hold on;
+end
+ylabel("Objective values");
+xlabel("Initial penalty value");
+legend(names);
+
+subplot(2,2,3); grid on; box on;
+for i = 1:length(names)
+    plot_struct = plot_maps(names{i});
+    loglog(rho, plot_struct.iter_vals); hold on;
+end
+ylabel("Number of iterations");
+xlabel("Initial penalty value");
+legend(names);
+
+subplot(2,2,4); grid on; box on;
+for i = 1:length(names)
+    plot_struct = plot_maps(names{i});
+    loglog(rho, plot_struct.outer_vals); hold on;
+end
+ylabel("Number of outer iterations");
+xlabel("Initial penalty value");
+legend(names);
+
+end
+
+% Objective versus angle
+function[] = ObjectiveVersusAngle(obj)
+
+figure; hold on; box on;
+
+% Number of linear segments approximating the circle
+N = 100;
+
+obj_vals = zeros(N, 1);
+angle_vals = zeros(N, 1);
+
+% Fill objective values
+vertices = [cos(2*pi*(1:N)/N); sin(2*pi*(1:N)/N)];
+for i = 1:N
+    angle_vals(i) = 2*pi*i/N;
+    obj_vals(i) = obj(vertices(:,i));
+end
+
+% Create the plot
+plot(angle_vals, obj_vals + 10);
+xlabel("angle of $x$");
+ylabel("objective value");
+
+% Set to log scale
+set(gca,'yscale','log')
+
+end
