19ff4a9fc7ed5f7ca139429cc7e0403d90f035e9
Jonas <hall.f.jonas@gmail.com>
2021-05-20 14:55:48 +0200

    feat: Clean up minimal example

--- interfaces/matlab/examples/warm_up.m
+++ interfaces/matlab/examples/warm_up.m
@@ -5,18 +5,11 @@ clear all; clc; close all;
 % Path to mex function (c++ solver)
 addpath(fullfile(pwd, ".."));
 
-% Path to LCQP and qpOASES (matlab solver)
-addpath("~/LCQP");
-addpath("~/qpOASES/interfaces/matlab");
-
 % Simple QP
 Q = [2 0; 0 2];
 g = [-2; -2];
 L = [1 0];
 R = [0 1];
-A = [1 -1];
-lbA = [-0.5];
-ubA = [inf];
 lb = [-inf; -inf];
 ub = [inf; inf];
 
@@ -24,29 +17,9 @@ ub = [inf; inf];
 params.x0 = [1; 1];
 params.initialPenaltyParameter = 0.01;
 params.penaltyUpdateFactor = 2;
-params.solveZeroPenaltyFirst = true;
-params.printLevel = 0;
 
 % Run solver
-nexp = 100;
-times_c = zeros(nexp, 1);
-times_m = zeros(nexp, 1);
-for i = 1:nexp
-    tic;
-    [xopt, yopt, stats] = LCQPanther(Q, g, L, R, A, lbA, ubA, lb, ub, params);
-    times_c(i) = toc;
+[x, y] = LCQPanther(Q, g, L, R, lb, ub, params);
 
-    tic;
-    LCQP(Q, g, L, R, A, lbA, ubA, lb, ub, params);
-    times_m(i) = toc;
-end
-
-% Plot results
-figure;
-subplot(1,2,1); hold on; box on;
-semilogy(times_c, 'r--', 'DisplayName', 'C++');
-semilogy(times_m, 'b--', 'DisplayName', 'MATLAB');
-
-subplot(1,2,2); hold on; box on;
-plot(times_m./times_c, '-', 'DisplayName', 'Speed Factor');
-plot(1:nexp, repmat(mean(times_m./times_c), nexp, 1), '-', 'DisplayName', 'Speed Factor');
+fprintf("x = [%g, %g]\n", x(1), x(2));
+fprintf("y = [%g, %g, %g, %g]\n", y(1), y(2), y(3), y(4));
