ca17d13c209acd9a02a443a37b9a8e3454c4009b
Jonas <hall.f.jonas@gmail.com>
2021-05-31 15:20:24 +0200

    test: Always return if a command fails

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# Break if any command fails
+set -e
+
+# Run unit tests
 build/bin/tests/RunUnitTests
+
+# Run all examples
 build/bin/examples/warm_up
 build/bin/examples/warm_up_sparse
 build/bin/examples/warm_up_w_A
