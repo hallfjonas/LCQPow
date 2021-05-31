1220e5a6d4278f6c04e130dffd1234246744979a
Jonas <hall.f.jonas@gmail.com>
2021-05-31 12:51:15 +0200

    feat: Don't require box constraints (they are set by the solver)

--- interfaces/matlab/examples/warm_up.m
+++ interfaces/matlab/examples/warm_up.m
@@ -10,16 +10,20 @@ Q = [2 0; 0 2];
 g = [-2; -2];
 L = [1 0];
 R = [0 1];
-lb = [-inf; -inf];
-ub = [inf; inf];
 
 % Algorithm parameters (C++)
 params.x0 = [1; 1];
 params.initialPenaltyParameter = 0.01;
 params.penaltyUpdateFactor = 2;
+params.qpSolver = 2;
 
 % Run solver
-[x, y] = LCQPanther(Q, g, L, R, lb, ub, params);
+[x, y] = LCQPanther(Q, g, L, R, params);
 
 fprintf("x = [%g, %g]\n", x(1), x(2));
-fprintf("y = [%g, %g, %g, %g]\n", y(1), y(2), y(3), y(4));
+
+if (length(y) == 4)
+    fprintf("y = [%g, %g, %g, %g]\n", y(1), y(2), y(3), y(4));
+elseif (length(y) == 2)
+    fprintf("y = [%g, %g]\n", y(1), y(2));
+end
