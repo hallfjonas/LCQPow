95b53f70ecf49bb768ff2d32968fd3846835a2b3
Jonas <hall.f.jonas@gmail.com>
2021-06-02 17:34:18 +0200

    doxy: Improve code documentation

--- include/SubsolverOSQP.hpp
+++ include/SubsolverOSQP.hpp
@@ -35,6 +35,11 @@ namespace LCQPanther {
 			/** Default constructor. */
 			SubsolverOSQP( );
 
+            /** Constructor for sparse matrices.
+             *
+             * @param H The Hessian matrix in sparse csc format.
+             * @param A The linear constraint matrix in sparse csc format (should include the rows of the complementarity selector matrices).
+            */
             SubsolverOSQP(  const csc* const _H,
                             const csc* const _A
                             );
@@ -57,14 +62,29 @@ namespace LCQPanther {
             /** Set print level. */
             void setPrintlevl( bool verbose );
 
-            /** Run OSQP solver. */
+            /** Abstract method for solving the QP.
+             *
+             * @param initialSolver A flag indicating whether the call should initialize the sequence.
+             * @param iterations A reference to write the number of subsolver iterates to.
+             * @param _g The (potentially) updated objective linear component.
+             * @param _lbA The (potentially) updated lower bounds of the linear constraints.
+             * @param _ubA The (potentially) updated upper bounds of the linear constraints.
+             * @param _x0 The primal initial guess. NULL pointer can be passed.
+             * @param _y0 The dual initial guess. NULL pointer can be passed.
+             * @param _lb This entry is ignored in this solver (only required in declaration due to inflexibility of abstract classes).
+             * @param _ub This entry is ignored in this solver (only required in declaration due to inflexibility of abstract classes).
+            */
             ReturnValue solve(  bool initialSolve, int& iterations,
                                 const double* const _g,
                                 const double* const _lbA, const double* const _ubA,
                                 const double* const x0 = 0, const double* const y0 = 0,
                                 const double* const _lb = 0, const double* const _ub = 0);
 
-            /** Write solution to x. */
+			/** Get the primal and dual solution.
+             *
+             * @param x Pointer to the (assumed to be allocated) primal solution vector.
+             * @param y Pointer to the (assumed to be allocated) dual solution vector.
+            */
             void getSolution( double* x, double* y );
 
         protected:
@@ -75,15 +95,15 @@ namespace LCQPanther {
             int nVars;                              /**< Number of optimization variables. */
             int nDuals;                             /**< Total number of dual variables. */
 
-            OSQPWorkspace *work = NULL;
-            OSQPSettings *settings = NULL;
-            OSQPData *data = NULL;
+            OSQPWorkspace *work = NULL;             /**< OSQP workspace. */
+            OSQPSettings *settings = NULL;          /**< OSQP settings. */
+            OSQPData *data = NULL;                  /**< OSQP data. */
 
-            csc* H = NULL;
-            csc* A = NULL;
-            c_float* g = NULL;
-            c_float* l = NULL;
-            c_float* u = NULL;
+            csc* H = NULL;                          /**< Hessian matrix in csc format. */
+            csc* A = NULL;                          /**< Constraint matrix in csc format (should contain rows of compl. sel. matrices). */
+            c_float* g = NULL;                      /**< Objectives linear component in dense format. */
+            c_float* l = NULL;                      /**< Lower bounds on A (should contain the bounds of the complementarity pairs). */
+            c_float* u = NULL;                      /**< Upper bounds on A (should contain the bounds of the complementarity pairs). */
     };
 }
 
