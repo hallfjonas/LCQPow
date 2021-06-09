6cacfc3331f0679ec2e4e46988f4075ea554b509
Jonas <hall.f.jonas@gmail.com>
2021-06-09 11:29:44 +0200

    feat: one job with one step and only build and run, no doxy etc

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 mkdir -p build
 cd build
-cmake -DBUILD_SHARED_LIBS=ON ..
+cmake ..
 make
 cd ..
--- run.sh
+++ run.sh
@@ -4,12 +4,11 @@
 set -e
 
 # Run unit tests
-build/bin/tests/RunUnitTests
+# build/bin/tests/RunUnitTests
 
 # Run all examples
 build/bin/examples/warm_up
-build/bin/examples/warm_up_sparse
+build/bin/examples/warm_up_schur
+build/bin/examples/warm_up_osqp
 build/bin/examples/warm_up_w_A
 build/bin/examples/OptimizeOnCircle
-
-doxygen build/Doxyfile.doxy
