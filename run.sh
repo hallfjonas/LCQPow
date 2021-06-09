d8641924759a7a4bf88d666046a51a5ed587ce71
Jonas <hall.f.jonas@gmail.com>
2021-06-09 13:12:15 +0200

    run: Run all examples in examples dir (independent of filename)

--- run.sh
+++ run.sh
@@ -5,10 +5,7 @@ set -e
 
 # Run unit tests
 # build/bin/tests/RunUnitTests
-
-# Run all examples
-build/bin/examples/warm_up
-build/bin/examples/warm_up_schur
-build/bin/examples/warm_up_osqp
-build/bin/examples/warm_up_w_A
-build/bin/examples/OptimizeOnCircle
+for f in ./build/bin/examples/*; do
+    echo "Running $f"
+    $f
+done
